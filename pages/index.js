af9186df52abe0447b2a7379d56ca0fe4a27f100
kharioki <antolisah@gmail.com>
2020-09-06 01:48:48 +0300

    added dependencies and stripe elements setup

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,56 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements
+} from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(
+  'pk_test_51HO9RkC1aJwwpXUQPTyh95N9JttTnGALXQDe72ZH6Hg40uqMIKTiEMdI8GYFD2yIBRUYOcx5noiZzrW0gqNr3JhG00rmyDDcIw'
+);
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement)
+    });
+
+    if (!error) {
+      console.log(paymentMethod);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{ maxWidth: '400px', margin: '0 auto' }}
+    >
+      <h2>Price: $10.99 USD</h2>
+      <img
+        src="https://images.ricardocuisine.com/services/recipes/277x375_7769.jpg"
+        alt="pita sandwiches"
+        style={{ maxWidth: '50px' }}
+      />
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
 
 export default function Index() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Next-stripe React</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Home sweet home
-    </div>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
   );
 }
